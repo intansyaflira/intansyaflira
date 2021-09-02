96b9635c768acaf66730554143632142b90a5574
Intan Dwi Syaflira <73926880+intansyaflira@users.noreply.github.com>
2021-09-02 13:05:54 +0700

    Add files via upload

---
+++ prosestambahproduk.php
@@ -0,0 +1,46 @@
+<?php
+
+if($_POST){
+
+    $nama_produk=$_POST['nama_produk'];
+
+    $deskripsi=$_POST['deskripsi'];
+
+    $harga=$_POST['harga'];
+
+    $foto_produk=$_POST['foto_produk'];
+
+    if(empty($nama_produk)){
+
+        echo "<script>alert('nama produk tidak boleh kosong');location.href='tambahproduk.php';</script>";
+
+
+    } elseif(empty($deskripsi)){
+
+        echo "<script>alert('deskripsi tidak boleh kosong');location.href='tambahproduk.php';</script>";
+
+    } elseif(empty($harga)) {
+        
+        echo "<script>alert('harga tidak boleh kosong');location.href='tambahproduk.php';</script>";
+    } 
+    else {
+
+        include "cekkoneksi.php";
+
+        $insert=mysqli_query($conn,"INSERT INTO produk (nama_produk, deskripsi, harga, foto_produk) VALUES ('".$nama_produk."','".$deskripsi."', '".$harga."', '".$foto_produk."')");
+
+        if($insert){
+
+            echo "<script>alert('Sukses menambahkan produk');location.href='tambahproduk.php';</script>";
+
+        } else {
+
+            echo "<script>alert('Gagal menambahkan produk');location.href='tambahproduk.php';</script>";
+
+        }
+
+    }
+
+}
+
+?>
